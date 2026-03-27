efe78fc507396f6cdc8631df76309384dff36672
Ankit Raj Chaturvedi <ankit.chaturvedi129217@marwadiuniversity.ac.in.com>
2026-03-27 18:42:51 +0530

    files addded

---
+++ UNIT04/Program01/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Program01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(20, 40);
+
+            rectangle.Calculate();
+            rectangle.Display();
+        }
+    }
+}
